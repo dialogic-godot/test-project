5f1cc68dfb32580a65213c49e829bac79c5f31ec
Jowan-Spooner <raban-loeffler@posteo.de>
2023-11-02 23:59:38 +0100

    Improvements across the board
    Fixes to the main menu.
    Fixes to the VisualNovel UI.
    Fixes to some timelines.
    Fixes to Player in SmallRPG example.
    Fixes to custom_background.
    
    Static typing improvements, trailing whitespace removal.

--- MainMenu/ProjectPreview.gd
+++ MainMenu/ProjectPreview.gd
@@ -6,39 +6,42 @@ extends Panel
 signal pressed
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	$Image.texture = image
-	$Image.pivot_offset = $Image.size/2
 	$ColorRect/Name.text = title
+	await get_tree().process_frame
+	$Image.pivot_offset = $Image.size/2
 
 
-func _gui_input(event):
+func _gui_input(_event:InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		pressed.emit()
 		accept_event()
 
-func _input(event):
-	if event is InputEventMouseButton and event.pressed and get_global_rect().has_point(get_global_mouse_position()):
+func _input(event:InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed \
+		and get_global_rect().has_point(get_global_mouse_position()) \
+		and is_visible_in_tree():
 		pressed.emit()
 
 
-func _on_color_rect_mouse_exited():
+func _on_color_rect_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_property($Image, 'scale', Vector2(1,1), 0.2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
 
-func _on_color_rect_mouse_entered():
+func _on_color_rect_mouse_entered() -> void:
 	$Image.pivot_offset = $Image.size/2
 	var tween := create_tween()
 	tween.tween_property($Image, 'scale', Vector2(1.1,1.1), 0.2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
 
-func _on_focus_entered():
+func _on_focus_entered() -> void:
 	_on_color_rect_mouse_entered()
 	$ColorRect/Name.modulate = Color.ORANGE
 
 
-func _on_focus_exited():
+func _on_focus_exited() -> void:
 	_on_color_rect_mouse_exited()
 	$ColorRect/Name.modulate = Color.WHITE
 
--- SmallRPG/TopDown_Interactable.gd
+++ SmallRPG/TopDown_Interactable.gd
@@ -18,13 +18,13 @@ func _ready():
 func _on_body_entered(body:PhysicsBody2D) -> void:
 	if body is CharacterBody2D:
 		var indicator := Sprite2D.new()
-		indicator.name = "InteractionIndicator" 
+		indicator.name = "InteractionIndicator"
 		indicator.texture = preload("res://SmallRPG/Assets/interaction.png")
 		indicator.modulate = indicator_color
 		indicator.scale = Vector2(10,10)/indicator.texture.get_size()
 		add_child(indicator)
 		indicator.position = Vector2(0,-10)
-		
+
 		_player = body
 
 
@@ -34,19 +34,19 @@ func _on_body_exited(body:PhysicsBody2D) -> void:
 			get_node('InteractionIndicator').queue_free()
 
 
-func _input(event:InputEvent):
+func _input(_event:InputEvent):
 	if Input.is_action_just_pressed("interact"):
 		if !has_node("InteractionIndicator"):
 			return
 		get_node('InteractionIndicator').queue_free()
-		
+
 		if has_node('Sprite') and _player:
 			get_node('Sprite').flip_h = _player.position.x < position.x
 			_player.get_node('Sprite').flip_h =  _player.position.x > position.x
 			_player.get_node('Camera2D').align()
 			_player.get_node('Camera2D').position = global_position-_player.global_position
 			_player.zoom_in()
-			
+
 		if timeline:
 			owner.state = owner.States.DIALOG
 			get_viewport().set_input_as_handled()
@@ -54,12 +54,12 @@ func _input(event:InputEvent):
 			var node := Dialogic.start(timeline, next_label)
 			if _player:
 				node.register_character(load("res://SmallRPG/Timelines/Player.dch"), _player.get_node('BubbleMarker'))
-			
+
 			if character:
 				node.register_character(character, $BubbleMarker)
-			
-			
-			Dialogic.timeline_ended.connect(_on_dialog_end, CONNECT_ONE_SHOT)
+
+
+			Dialogic.timeline_ended.connect(_on_dialog_end)
 			if not Dialogic.signal_event.is_connected(_on_dialogic_signal_event):
 				Dialogic.signal_event.connect(_on_dialogic_signal_event)
 			counter += 1
