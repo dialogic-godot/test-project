bf6aa0caac286a3b018cdfd9af726fe4a295a97c
Jowan-Spooner <raban-loeffler@posteo.de>
2024-10-27 13:12:39 +0100

    Content changes

--- FakeUnitTests/fake_unit_test_scene.gd
+++ FakeUnitTests/fake_unit_test_scene.gd
@@ -4,5 +4,3 @@ extends Control
 func _ready() -> void:
 	Dialogic.Styles.load_style("VisualNovel_Style")
 	Dialogic.start("t0_overview")
-
-
--- FakeUnitTests/unit_test_autoload.gd
+++ FakeUnitTests/unit_test_autoload.gd
@@ -3,4 +3,3 @@ extends Node
 var var_float := 2.0
 var var_string := "Hello World"
 var var_array := ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-
