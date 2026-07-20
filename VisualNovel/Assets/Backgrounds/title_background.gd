1eeb28d33659380bcd0a38eeb1ac5531e3568bc2
Jowan-Spooner <raban-loeffler@posteo.de>
2026-07-20 19:28:11 +0200

    Various changes to accompany alpha 20

--- VisualNovel/Assets/Backgrounds/title_background.gd
+++ VisualNovel/Assets/Backgrounds/title_background.gd
@@ -1,7 +1,13 @@
 extends DialogicBackground
 
 
+func _ready() -> void:
+	print("\nNEW TITLE SCENE")
+	#_update_background("[fancy_in]WOWIEEEEE", 0.0)
+
 func _update_background(argument:String, _time:float) -> void:
+	await get_tree().create_timer(1).timeout
+	print("showing title: ", argument)
 	$Title.set_speed(0.08)
 	$Title.reveal_text(argument)
 	Dialogic.Text.active_textbox = "main"
