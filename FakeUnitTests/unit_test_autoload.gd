ff27dc4efe35790486f706ca2702eb42c92cacc8
Jowan-Spooner <raban-loeffler@posteo.de>
2026-08-11 10:41:51 +0200

    Add many more tests for portraits
    (Some of it is a bit broken because it was designed to test functionality from the PortraitContainerMovementPR. I will update the dialogic branch next.

--- FakeUnitTests/unit_test_autoload.gd
+++ FakeUnitTests/unit_test_autoload.gd
@@ -3,3 +3,7 @@ extends Node
 var var_float := 2.0
 var var_string := "Hello World"
 var var_array := ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+
+
+func set_container_debug_draw(enabled:bool):
+	Dialogic.PortraitContainers.debug_draw = enabled
